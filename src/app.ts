4c6318499652d87236f522227682627421c1d183
Danil Katuhin <the.darkvood@mail.ru>
2019-12-04 22:49:02 +0300

    feat(app): Add "seed" validation

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import { AppParams, FoxlDBCore } from "./core/core";
+import { isObject } from "./core/utils";
 
 export class FoxlDB {
   private $app: FoxlDBCore;
 
   constructor({ path, save = true, seed = {} }: AppParams) {
     if (!path && typeof path !== "string") {
-      throw new Error(`[FoxlDB] The property "path" must be non-empty string`);
+      throw new Error(`[FoxlDB] The params "path" must be non-empty string`);
     }
 
     if (typeof save !== "boolean") {
-      throw new Error(`[FoxlDB] The property "save" must be boolean`);
+      throw new Error(`[FoxlDB] The params "save" must be boolean`);
+    }
+
+    if (!isObject(seed)) {
+      throw new Error(`[FoxlDB] The params "seed" must be plain object`);
     }
 
     this.$app = new FoxlDBCore({ path, save, seed });
