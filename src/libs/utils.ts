8a4d36b493dc0741687b0ab403823a0dc80bcd88
Danil Katuhin <the.darkvood@mail.ru>
2019-12-08 21:45:44 +0300

    fix(storage): safeGet() can't get prototype properties now

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -5,7 +5,14 @@ export function isObject(value: any) {
 }
 
 export function safeGet(obj: any, path: string, defaultValue = noDefault) {
-  const result = path.split(".").reduce((acc, curr) => (acc && typeof acc === "object" ? acc[curr] : undefined), obj);
+  const result = path.split(".").reduce((acc, curr) => {
+    if (acc && typeof acc === "object" && !Array.isArray(acc) && acc.hasOwnProperty(curr)) {
+      return acc[curr];
+    } else {
+      return undefined;
+    }
+  }, obj);
+
   const hasDefault = defaultValue !== noDefault;
 
   if (result === undefined && hasDefault) {
